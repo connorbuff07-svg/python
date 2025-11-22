b9c53ef37ae3d6ee62d29b8dd22f0ed3bb85adda
connorbuff07-svg <connorbuff07@gmail.com>
2025-11-21 20:43:50 -0600

    television.py

--- television.py
+++ television.py
@@ -10,42 +10,42 @@ class Television:
         self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
         if self.__status:
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         if self.__status:
             if self.__channel >= self.MAX_CHANNEL:
                 self.__channel = self.MIN_CHANNEL
             else:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         if self.__status:
             if self.__channel <= self.MIN_CHANNEL:
                 self.__channel = self.MAX_CHANNEL
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         if self.__status:
             if self.__muted:
                 self.__muted = False
             if self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         if self.__status:
             if self.__muted:
                 self.__muted = False
             if self.__volume > self.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         volume = self.__volume
         if self.__muted:
             volume = 0
